efa55b7d9108f38b53b65faec48e42c8dc901b9c
Steve <steve.gackstetter@gmail.com>
2021-02-17 10:19:23 -0600

    Now sending HTML using sendFile, created app.jsx for react comps

---
+++ client/app.jsx
@@ -0,0 +1,11 @@
+{/* <form>
+<label for="name">Name</label>
+<input type="textarea" name="name">
+<label for="email">Email</label>
+<input type="textarea" name="email">
+<label for="password">Password</label>
+<input type="textarea" name="password">
+<br>
+<br>
+<input type="submit">
+</form> */}
--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 let app = express();
 let port = 3002;
+
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'), (err) => {
+    if (err) {
+      console.log('error');
+    } else {
+      console.log('file sent');
+    }
+  })
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
